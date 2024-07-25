0412d4f7b48891c19dac9ae46a8bc94491492010
yrebahpro <yoann.rebah.pro@gmail.com>
2024-07-25 20:16:27 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, PLATFORM_ID, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeRouteComponent } from '../routes/home-route/home-route.component';
 import { ThemeService } from '../services/theme.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -17,12 +18,16 @@ import { ThemeService } from '../services/theme.service';
 export class AppComponent implements OnInit {
   themeService = inject(ThemeService);
   
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
     this.themeService.init();
     setTimeout(()=>{
-      console.log(this.themeService.theme)
+      if (isPlatformBrowser(this.platformId)) {
+        const htmlElement = document.documentElement;
+        htmlElement.setAttribute('data-theme', this.themeService.theme);
+        console.log(this.themeService.theme)
+      }
     }, 0)
   }
 }
